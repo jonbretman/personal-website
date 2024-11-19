0db16315c461830c58faf5f1395741f00f6b94d6
Jon Bretman <jon.bretman@gmail.com>
2024-11-19 08:59:54 +0000

    Add some opengraph data

--- app/routes/posts.$slug.tsx
+++ app/routes/posts.$slug.tsx
@@ -1,9 +1,38 @@
-import { LoaderFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { LoaderFunction, MetaArgs } from "@remix-run/node";
+import { MetaFunction, useLoaderData } from "@remix-run/react";
 import { getPost, PostData } from "../posts.server";
 
 type LoaderData = {
   post: PostData;
+};
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  const { post } = data;
+  return [
+    {
+      title: post.attributes.title,
+    },
+    {
+      name: "og:title",
+      content: post.attributes.title,
+    },
+    {
+      name: "og:type",
+      content: "article",
+    },
+    {
+      name: "og:url",
+      content: `https://www.jonbretman.com/posts/${post.slug}`,
+    },
+    {
+      name: "og:image",
+      content: `https://www.jonbretman.com/photos/2022-08-17-me.jpeg`,
+    },
+    {
+      name: "og:site_name",
+      content: `Jon Bretman`,
+    },
+  ];
 };
 
 export const loader: LoaderFunction = async ({
